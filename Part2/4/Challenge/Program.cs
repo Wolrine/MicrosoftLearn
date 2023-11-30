c627bf015bf33c9ff3e97eef725278643e83c29f
Wolrine <volam030102@gmail.com>
2023-11-30 21:55:36 +0700

    3 learn challenge

---
+++ Part2/4/Challenge/Program.cs
@@ -0,0 +1,7 @@
+﻿String[] OrderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach(String orderID in OrderIDs) {
+    if(orderID.StartsWith("B")) {
+        Console.WriteLine(orderID);
+    }
+}
